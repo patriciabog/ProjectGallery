a06626a6e5245112f386f043655d69735679afd3
Patricia <patriciamvbog@gmail.com>
2023-06-28 22:51:00 +0100

    add imports to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,30 @@
-/* SECCIÓN DE IMPORT */
+import "../styles/App.scss";
+import {Route, Routes} from 'react-router-dom';
+import Landing from "./Landing";
+import CreateProject from "./CreateProject"; 
+import Error404 from "./Error404";
+import ls from '../services/localStorage';
+import { useState } from "react";
 
-// - De React
-// - Nuestros
-// - Sass
-import '../styles/App.scss';
-// - Imágenes
 
-/* SECCIÓN DEL COMPONENTE */
 function App() {
- 
+  const [allCards, setAllCards] = useState (ls.get('projectsLS', [])); 
 
-  /* HTML */
-  return <div className="App">{/* Aquí va el HTML */}</div>;
-}
+  const handleLs = (value) => {
+    setAllCards(value)
+  }
 
-/* PROP-TYPES */
 
-/* EXPORT DEL COMPONENTE */
+  return(
+    <Routes>
+      <Route path="/" element={<Landing allCards={allCards} />}>
+      <Route path="/*" element={<Error404 />}></Route>
+      </Route>
+      <Route path="/create" element={<CreateProject allCards={allCards} handleLs={handleLs} />}></Route>
+      <Route path="/create/*" element={<Error404 />}></Route>
+    </Routes>
+
+  );
+};
+
 export default App;
