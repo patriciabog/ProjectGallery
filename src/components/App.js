d710f5624dd7244b984189b91bb0f8699e01bb22
Patricia <patriciamvbog@gmail.com>
2023-06-28 19:41:00 +0100

    add template

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+/* SECCIÓN DE IMPORT */
+
+// - De React
+// - Nuestros
+// - Sass
+import '../styles/App.scss';
+// - Imágenes
+
+/* SECCIÓN DEL COMPONENTE */
+function App() {
+ 
+
+  /* HTML */
+  return <div className="App">{/* Aquí va el HTML */}</div>;
+}
+
+/* PROP-TYPES */
+
+/* EXPORT DEL COMPONENTE */
+export default App;
