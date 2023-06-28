3233e2946a5387d2162dbd2bf68a0916ad972b03
Patricia <patriciamvbog@gmail.com>
2023-06-28 23:04:03 +0100

    add images

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import "../styles/App.scss";
 import {Route, Routes} from 'react-router-dom';
 import Landing from "./Landing";
 import CreateProject from "./CreateProject"; 
-import Error404 from "./Error404";
+
 import ls from '../services/localStorage';
 import { useState } from "react";
 
@@ -18,10 +18,8 @@ function App() {
   return(
     <Routes>
       <Route path="/" element={<Landing allCards={allCards} />}>
-      <Route path="/*" element={<Error404 />}></Route>
       </Route>
       <Route path="/create" element={<CreateProject allCards={allCards} handleLs={handleLs} />}></Route>
-      <Route path="/create/*" element={<Error404 />}></Route>
     </Routes>
 
   );
